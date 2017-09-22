3ea4861b5d04d671d82b1abd6aec1b453b5dbbf4
Gabriel Lanata <gabriellanata@gmail.com>
2017-09-22 17:04:50 -0500

    Version Update

--- Tests/StartAppsKitAnimationsTests/StartAppsKitAnimationsTests.swift
+++ Tests/StartAppsKitAnimationsTests/StartAppsKitAnimationsTests.swift
@@ -2,10 +2,11 @@ import XCTest
 @testable import StartAppsKitAnimations
 
 class StartAppsKitAnimationsTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(StartAppsKitAnimations().text, "Hello, World!")
+        // XCTAssertEqual(StartAppsKitAnimations().text, "Hello, World!")
     }
 
 
